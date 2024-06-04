815524d2bc791166ba977ae3bb44dca7f13999a1
Geremia Taglialatela <tagliala.dev@gmail.com>
2024-06-04 18:46:56 +0200

    Move to GitHub actions

--- install/imagemagick.sh
+++ install/imagemagick.sh
@@ -1,7 +1,7 @@
 set -ex
 
-im_download_path=$(curl -sf https://download.imagemagick.org/ImageMagick/download/releases/ | grep -o "ImageMagick-$IM_VERSION-.[0-9]*.tar.xz" -m 1 | head -1)
-curl -f "https://download.imagemagick.org/ImageMagick/download/releases/$im_download_path" > ImageMagick.tar.xz
+im_download_path=$(curl -sf https://download.imagemagick.org/archive/releases/ | grep -o "ImageMagick-$IM_VERSION-.[0-9]*.tar.xz" | tail -1)
+curl -f "https://download.imagemagick.org/archive/releases/$im_download_path" > ImageMagick.tar.xz
 tar xf ImageMagick.tar.xz
 cd ImageMagick-*
 ./configure --prefix=/usr
